254aa3ffe6a28b775ec4d792f4ca8a9d8e6681ee
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-05 22:36:58 -0700

    first comit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,20 @@
+class DemoController < ApplicationController
+   
+  def index
+  	# render(:template => 'demo/hello')
+  end
+
+  def hello
+  	# render('index')
+    @id = params['id']
+    @page = params[:page]
+  end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def lynda
+  	redirect_to("http://lynda.com")
+  end
+end
