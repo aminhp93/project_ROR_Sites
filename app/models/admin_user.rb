0424d4132405ff96977b08daf42c858a7aaed6a0
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-06 01:49:56 -0700

    Done with Association"

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -12,4 +12,8 @@ class AdminUser < ActiveRecord::Base
 	# def last_name=(value)
 	# 	@last_name =value
 	# end
+
+	has_and_belongs_to_many :pages
+	has_many :section_edits
+	has_many :sections, :through => :section_edits
 end
