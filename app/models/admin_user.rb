d187087af956c5e4bde63fda42604680449ac429
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-10 01:33:34 -0700

    Validates method and custom method

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -19,16 +19,33 @@ class AdminUser < ActiveRecord::Base
 
 	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
 
-	validates_presence_of :first_name
-	validates_length_of :first_name, :maximum => 25
-	validates_presence_of :last_name
-	validates_length_of :last_name, :maximum => 50
-	validates_presence_of :username
-	validates_length_of :username, :within => 8..25
-	validates_uniqueness_of :username
-	validates_presence_of :email
-	validates_length_of :email, :maximum => 100
-	validates_format_of :email, :with => EMAIL_REGEX
-	validates_confirmation_of :email
+	FORBIDDEN_USERNAMES = ['little', 'aloha']
+
+	# validates_presence_of :first_name
+	# validates_length_of :first_name, :maximum => 25
+	# validates_presence_of :last_name
+	# validates_length_of :last_name, :maximum => 50
+	# validates_presence_of :username
+	# validates_length_of :username, :within => 8..25
+	# validates_uniqueness_of :username
+	# validates_presence_of :email
+	# validates_length_of :email, :maximum => 100
+	# validates_format_of :email, :with => EMAIL_REGEX
+	# validates_confirmation_of :email
+
+	# shortcut validation, aka "sexy validation"
+
+	validates :first_name, :presence => true, :length => {:maximum => 25}
+	validates :last_name, :presence => true, :length => {:maximum => 50}
+	validates :username, :presence => true, :length => {:within => 8..25}, :uniqueness => true
+	validates :email, :presence => true, :length => {:maximum => 100}, :format => EMAIL_REGEX, :confirmation => true
+
+	validate :username_is_allowed
+
+	def username_is_allowed
+		if FORBIDDEN_USERNAMES.include?(username)
+			errors.add(:username, "has been restricted from use")
+		end
+	end
 
 end
